94ba798214dd315f27d0453110e4bc65440918a7
Skip R <tinyslices@gmail.com>
2024-03-24 02:40:51 -0700

    remove kotlin and entrypoints

--- src/client/kotlin/house/skip/cocoabeans/CocoaBeansClient.kt
+++
@@ -1,9 +0,0 @@
-package house.skip.cocoabeans
-
-import net.fabricmc.api.ClientModInitializer
-
-object CocoaBeansClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
